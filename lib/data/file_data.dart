3897230e8807f98718fc59177dd6d9700d62bc6e
madhurima.dey <madhurima.dey@keross.com>
2024-12-23 17:58:46 +0530

    Home page update

--- lib/data/file_data.dart
+++ lib/data/file_data.dart
@@ -4,6 +4,7 @@ final List<FileItem> items = [
   FileItem(name: "Documents", icon: "assets/folder.svg", isFolder: true),
   FileItem(name: "Photos", icon: "assets/folder.svg", isFolder: true),
   FileItem(name: "File1.pdf", icon: "assets/pdf-file.svg", isFolder: false),
-  FileItem(name: "File2.png", icon: "assets/file-file.svg", isFolder: false),
+  FileItem(name: "File2.png", icon: "assets/png-file.svg", isFolder: false),
+  FileItem(name: "File3.txt", icon: "assets/file-file.svg", isFolder: false),
   // Add more items as needed
 ];
